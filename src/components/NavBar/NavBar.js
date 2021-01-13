502671796cc55730adedff84b6a7f9adef60b1fc
joellekado <jkado@my.hpu.edu>
2021-01-13 14:54:34 -0600

    gridlines created

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import {AppBar, Toolbar} from '@material-ui/core'
+import {AppBar, Toolbar, Grid} from '@material-ui/core'
 
-export default function NavBar(params) {
+export default function NavBar() {
     return(
         
-       <AppBar position="static">   <Toolbar></Toolbar>     </AppBar>
+       <AppBar position="static">   
+       <Toolbar>
+           <Grid container>
+            <Grid item sm={6} style={{border:'1px solid #fff'}}>
+
+            </Grid>
+
+            <Grid item sm={6} style={{border: '1px solid magenta'}}>
+
+</Grid>
+
+
+           </Grid>
+           
+           </Toolbar>     
+           </AppBar>
     )
 }
