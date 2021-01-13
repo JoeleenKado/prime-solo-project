67d93a5379cd14c0708d0b153ab61081e34060f3
joellekado <jkado@my.hpu.edu>
2021-01-13 15:22:01 -0600

    notification icon in NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import {AppBar, Toolbar, Grid} from '@material-ui/core'
+import {AppBar, Toolbar, Grid, Badge, IconButton} from '@material-ui/core'
+//styling
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+
+
 
 export default function NavBar() {
     return(
@@ -8,11 +12,17 @@ export default function NavBar() {
        <Toolbar>
            <Grid container>
             <Grid item sm={6} style={{border:'1px solid #fff'}}>
+                {/*  input base is the search bar i think*/}               
+<inputBase/>
 
             </Grid>
 
             <Grid item sm={6} style={{border: '1px solid magenta'}}>
-
+       <IconButton>
+           <Badge badgeContent={4} color="secondary">
+               <NotificationsNoneIcon/>
+           </Badge>
+       </IconButton>
 </Grid>
 
 
