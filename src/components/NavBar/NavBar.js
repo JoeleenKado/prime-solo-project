0bad48fa89b5926923e5fa1ef0594f8bb17bb234
joellekado <jkado@my.hpu.edu>
2021-01-13 15:38:29 -0600

    logout icon on navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,8 +2,8 @@ import React from 'react';
 import {AppBar, Toolbar, Grid, Badge, IconButton} from '@material-ui/core'
 //styling
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
-
-
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import MeetingRoomIcon from '@material-ui/icons/MeetingRoom';
 
 export default function NavBar() {
     return(
@@ -11,20 +11,32 @@ export default function NavBar() {
        <AppBar position="static">   
        <Toolbar>
            <Grid container>
+           
+           {/* start grid item */}
             <Grid item sm={6} style={{border:'1px solid #fff'}}>
                 {/*  input base is the search bar i think*/}               
 <inputBase/>
-
-            </Grid>
-
+           </Grid>
+{/* end griditem */}
+        
+        {/* start grid item */}
             <Grid item sm={6} style={{border: '1px solid magenta'}}>
        <IconButton>
-           <Badge badgeContent={4} color="secondary">
+           <Badge badgeContent={3} color="secondary">
                <NotificationsNoneIcon/>
            </Badge>
+           </IconButton>
+           <IconButton>
+           <Badge badgeContent={4} color="secondary">
+               <ChatBubbleOutlineIcon/>
+           </Badge>
        </IconButton>
-</Grid>
+       <IconButton>
+               <MeetingRoomIcon/>
+       </IconButton>
 
+        </Grid>
+{/* end grid item */}
 
            </Grid>
            
