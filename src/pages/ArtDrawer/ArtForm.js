dcad2873b4708d4aadb92656bc7d410f6205b929
joellekado <jkado@my.hpu.edu>
2021-01-14 23:15:51 -0600

    form inputs on dom

--- src/pages/ArtDrawer/ArtForm.js
+++ src/pages/ArtDrawer/ArtForm.js
@@ -1,7 +1,65 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {AppBar, Toolbar, Grid, Badge, IconButton, makeStyles, InputBase, TextField} from '@material-ui/core'
+
+
+const initialFValues = {
+    title: '',
+    medium: '',
+    dimension: '',
+    url: '',
+    statement: ''
+}
 
 export default function ArtForm() {
+    const [values, setValues] = useState(initialFValues);
+
     return(
-       <div>artform</div>
+       <form>
+           <Grid container>
+               
+               <Grid item xs={2.0}>
+                   <TextField
+                   variant="outlined"
+                   label="Title"
+                   value={values.title}
+                   />
+                </Grid>
+
+                <Grid item xs={2.0}>
+                   <TextField
+                   variant="outlined"
+                   label="Medium"
+                   value={values.medium}
+                   />
+                </Grid>
+
+                <Grid item xs={2.0}>
+                   <TextField
+                   variant="outlined"
+                   label="Dimensions"
+                   value={values.dimension}
+                   />
+                </Grid> 
+
+                <Grid item xs={2.0}>
+                   <TextField
+                   variant="outlined"
+                   label="URL"
+                   value={values.url}
+                   />
+                </Grid>
+
+                <Grid item xs={2.0}>
+                   <TextField
+                   variant="outlined"
+                   label="Medium"
+                   value={values.medium}
+                   />
+                </Grid>
+
+           
+           
+           </Grid>
+       </form>
     )
 }
