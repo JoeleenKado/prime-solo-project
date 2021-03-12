cf4d1098fdd948d277c24428c9695a7a1cf99891
joellekado <jkado@my.hpu.edu>
2021-03-12 09:29:41 -0600

    changes for deployment

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -30,9 +30,9 @@ if (process.env.DATABASE_URL) {
   config = {
     user: process.env.PG_USER || null, //env var: PGUSER
     password: process.env.DATABASE_SECRET || null, //env var: PGPASSWORD
-    host: process.env.DATABASE_SERVER || 'localhost', // Server hosting the postgres database
-    port: process.env.DATABASE_PORT || 5432, // env var: PGPORT
-    database: process.env.DATABASE_NAME || 'virtual_gallery', // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
+    host: 'localhost', // Server hosting the postgres database
+    port: 5432, // env var: PGPORT
+    database: 'virtual_gallery', // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
   };
