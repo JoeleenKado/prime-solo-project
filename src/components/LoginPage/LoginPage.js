9226cbda2a146d9c60f50d144e1522d6b82373d0
joellekado <jkado@my.hpu.edu>
2022-01-31 21:27:44 -0600

    masterpieces framed

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -2,19 +2,22 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import mapStoreToProps from "../../redux/mapStoreToProps";
 import LoginForm from "../LoginForm/LoginForm";
-
+import './LoginPage.css'
 function LoginPage(props) {
 useEffect(() => {
 props.dispatch({type: 'FETCH_DATA'})
 }, [])
-const imageId = props.store.artic.image_id
+const {image_id} = props.store.artic
 // console.log('1111', props.store.artic.randomArt.image_id)
 // console.log('222', props.store.artic.imageId)
     return (
       <div>
         {JSON.stringify(props.store)}
-        {!imageId ? (<h2>Loading Artwork...</h2>) : 
-        (<img src={`https://www.artic.edu/iiif/2/${imageId}/full/843,/0/default.jpg`} alt="random artwork"/>
+        {!image_id ? (<h2>Loading Artwork...</h2>) : 
+        (<img 
+         className={'frame', 'border-one'} 
+        src={`https://www.artic.edu/iiif/2/${image_id}/full/843,/0/default.jpg`} 
+        alt="random artwork"/>
 )}
         <LoginForm />
 
