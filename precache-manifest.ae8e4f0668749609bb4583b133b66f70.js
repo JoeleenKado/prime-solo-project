3460eeb32022060aa2c05e39a5a21b108aaf2af5
joellekado <jkado@my.hpu.edu>
2022-03-19 23:17:37 -0500

    Updates

---
+++ precache-manifest.ae8e4f0668749609bb4583b133b66f70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de54eb70c2e0071645314a69c3bc0929",
+    "url": "/virtual-gallery/index.html"
+  },
+  {
+    "revision": "bb73ad269fb93635cae1",
+    "url": "/virtual-gallery/static/css/2.24daf3d1.chunk.css"
+  },
+  {
+    "revision": "e3023c9de623b4ab0916",
+    "url": "/virtual-gallery/static/css/main.22253d38.chunk.css"
+  },
+  {
+    "revision": "bb73ad269fb93635cae1",
+    "url": "/virtual-gallery/static/js/2.e053e10a.chunk.js"
+  },
+  {
+    "revision": "d96afa7375eea588b05af5d7cde5ce80",
+    "url": "/virtual-gallery/static/js/2.e053e10a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3023c9de623b4ab0916",
+    "url": "/virtual-gallery/static/js/main.fe5d5127.chunk.js"
+  },
+  {
+    "revision": "b337e697082c22fbc8fd",
+    "url": "/virtual-gallery/static/js/runtime-main.0af33611.js"
+  }
+]);
